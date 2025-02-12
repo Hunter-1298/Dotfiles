5d2e99e7f8c98c8f379355327b9a87733ea11ec0
Hunter Hayden <hunterhayden@MacBook-Pro-3.local>
2025-02-12 15:29:50 -0500

    Adding settings for vscode

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,17 +14,23 @@ require 'options'
 
 -- [[ Basic Keymaps ]]
 require 'keymaps'
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 require 'lazy-bootstrap'
 
--- [[ Configure and install plugins ]]
-require 'lazy-plugins'
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
-vim.cmd [[
-  augroup SetCmpSelHighlight
-    autocmd!
-    autocmd VimEnter * lua vim.api.nvim_set_hl(0, 'CmpSel', { bg = '#4CAF50', fg = '#2E3440', bold = true })
-  augroup END
-]]
+if vim.g.vscode then
+  require('lazy').setup {
+    require 'custom.plugins.flash',
+  }
+  vim.cmd [[source $HOME/.config/nvim/vscode/settings.vim]]
+else
+  -- [[ Configure and install plugins ]]
+  require 'lazy-plugins'
+  -- The line beneath this is called `modeline`. See `:help modeline`
+  -- vim: ts=2 sts=2 sw=2 et
+  vim.cmd [[
+    augroup SetCmpSelHighlight
+      autocmd!
+      autocmd VimEnter * lua vim.api.nvim_set_hl(0, 'CmpSel', { bg = '#4CAF50', fg = '#2E3440', bold = true })
+    augroup END
+  ]]
+end
