e5ebca3c14a9e6bae9aeda86ee942233e2fc3e2b
hhayden <hayden1298@hotmail.com>
2025-11-15 00:34:51 -0500

    reworking to use the new built in package manager. Going for minimilistic

--- nvim/after/ftplugin/go.lua
+++
@@ -1,8 +0,0 @@
-local bufnr = vim.api.nvim_get_current_buf()
---go nvim keybinding
-vim.api.nvim_set_keymap(
-  'n', -- Insert mode
-  '<leader>g', -- Keybinding (Ctrl+e, or choose your own)
-  'oif err !=  nil {<CR><TAB>fmt.Println("err: %v", err)<ESC>}', -- Opens a new line and inserts the snippet with newlines
-  { noremap = true, silent = true, desc = 'Go error' }
-)
--- nvim/lua/custom/plugins/rust_lang.lua
+++
@@ -1,6 +0,0 @@
---adding rust tools, make sure rust-analyzer is installed in local toolchain
-return {
-  {
-    'rust-lang/rust.vim',
-  },
-}
